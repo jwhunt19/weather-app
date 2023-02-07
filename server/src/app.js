4ea0e1d35b9f1aaed181f6df6f17ea9a8be4969b
Joey Huntington <jwhunt19@gmail.com>
2023-02-07 16:55:50 -0700

    setup handlebars js, replace static html with views

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,44 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '../public')))
+// Define paths for Express config
+const publicPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicPath))
+
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'jwhunt19'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'jwhunt19'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'this is an example message',
+    title: 'Help',
+    name: 'jwhunt19'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
